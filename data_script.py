ae2e03f0c8ec02998016bf08b4a81056a45eb575
Yiqin Huang <yh_739@discovery2.hpc.usc.edu>
2024-06-25 00:09:45 -0700

    cuda mem error

--- data_script.py
+++ data_script.py
@@ -4,43 +4,48 @@ import pandas as pd
 import numpy as np
 from tqdm.auto import tqdm
 
+from transformers import (AutoModelForCausalLM,
+                          AutoTokenizer)
 import os
 import json
 import torch
-import transformers
 
 def load_model(model_id, cache_dir):
     config_data = json.load(open('config.json'))
     os.environ['HF_TOKEN'] = config_data["HF_TOKEN"]
 
-    pipeline = transformers.pipeline(
-        "text-generation",
-        model=model_id,
-        model_kwargs={"torch_dtype": torch.bfloat16},
+    tokenizer = AutoTokenizer.from_pretrained(model_id, cache_dir=cache_dir)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id,
+        torch_dtype=torch.bfloat16,
         device_map="auto",
-        cache_dir=cache_dir
-    )
+        cache_dir=cache_dir)
+    model.eval()
+    return model, tokenizer
 
-    return pipeline
 
-
-def infer(pipeline, messages):
-
-    terminators = [
-        pipeline.tokenizer.eos_token_id,
-        pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>")
-    ]
-        
-    outputs = pipeline(
+def infer(model, tokenizer, messages):
+    input_ids = tokenizer.apply_chat_template(
         messages,
+        tokenize=True,
+        add_generation_prompt=True,
+        return_tensors="pt"
+        ).to('cuda')
+        
+    terminators = [
+        tokenizer.eos_token_id,
+        tokenizer.convert_tokens_to_ids("<|eot_id|>")]
+        
+    outputs = model.generate(
+        input_ids,
         max_new_tokens=1024,
         eos_token_id=terminators,
         do_sample=True,
         temperature=0.6,
         top_p=0.9,
-        )
-    
-    return outputs[0]["generated_text"][-1]
+)
+    response = outputs[0][input_ids.shape[-1]:-1]
+    return tokenizer.decode(response, skip_special_tokens=True)
 
 if __name__ == "__main__":
 
@@ -78,8 +83,8 @@ if __name__ == "__main__":
         json_str = one_article[['sent_lists', 'attributions']].to_json(lines=True, orient='records')
         articles.append((url, json_str))
 
-    #load the model pipeline
-    pipeline = load_model("meta-llama/Meta-Llama-3-70B-Instruct", "/project/jonmay_231/spangher/huggingface_cache")
+    #load the model
+    model, tokenizer = load_model("meta-llama/Meta-Llama-3-70B-Instruct", "/project/jonmay_231/spangher/huggingface_cache")
 
     # loop through and create prompts for each article
     for article in articles:
@@ -106,12 +111,13 @@ if __name__ == "__main__":
             "content": prompt},
             ]
         
-        response = infer(pipeline, message)
+        response = infer(model, tokenizer, message)
+
         with open('output.txt', 'a') as file:
-            file.write('url')
+            file.write(url)
             file.write('\n')
             file.write('{')
-            file.write('response')
+            file.write(response)
             file.write('}')
             file.write('\n')
-
+            file.write('\n')
--- datascript.sh
+++ datascript.sh
@@ -2,13 +2,11 @@
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH --time=40:00:00
-#SBATCH --gres=gpu:2
-#SBATCH --mem-per-gpu=100GB
+#SBATCH --gpus-per-task=a100:2
+#SBATCH --constraint=a100-80gb
 #SBATCH --cpus-per-gpu=10
 #SBATCH --partition=gpu
 
-module load conda
-source conda activate CIR
-pip install -r requirements.txt
 module load python/3.11
+pip install -r requirements.txt
 python3 data_script.py

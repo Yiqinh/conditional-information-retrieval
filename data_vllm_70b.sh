6c3c26b32b4f026b630fe350ce036ce415392419
Yiqin Huang <yh_739@discovery2.hpc.usc.edu>
2024-06-26 22:13:27 -0700

    implement vllm

--- data_vllm_70b.sh
+++ data_vllm_70b.sh
@@ -2,11 +2,12 @@
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH --time=40:00:00
-#SBATCH --gres=gpu:2
-#SBATCH --mem-per-gpu=80GB
+#SBATCH --gres=gpu:a100:2
+#SBATCH --constraint=a100-80gb
 #SBATCH --cpus-per-gpu=10
+#SBATCH --mem=100G
 #SBATCH --partition=gpu
 
 module load python/3.11
 pip install -r requirements.txt
-python3 data_vllm.py
+python3 data_vllm_70b.py

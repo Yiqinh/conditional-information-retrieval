3f4bb23ef6490ac379deb747c0b5de6946635f5c
Alex Spangher <spangher@usc.edu>
2024-06-29 02:18:01 -0400

    updated

--- data_vllm_70b.py
+++ data_vllm_70b.py
@@ -7,6 +7,13 @@ from tqdm.auto import tqdm
 import os
 import json
 import torch
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(lineno)d - %(message)s",
+    datefmt="%m/%d/%Y %H:%M:%S",
+    level=logging.INFO,
+)
 
 from vllm import LLM,  SamplingParams
 from transformers import AutoTokenizer
@@ -36,16 +43,28 @@ def infer(model, messages):
 
     return output[0].outputs[0].text
 
+
 if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, default="meta-llama/Meta-Llama-3-70B-Instruct")
+    parser.add_argument('--data_dir', type=str, default='./data')
+    parser.add_argument('--start_idx', type=int, default=None)
+    parser.add_argument('--end_idx', type=int, default=None)
+    args = parser.parse_args()
+
 
     #load in the data
-    data_dir = './data'
-    source_df = pd.read_json(f'{data_dir}/full-source-scored-data.jsonl.gz', lines=True, compression='gzip', nrows=100)
+    source_df = pd.read_json(
+        f'{args.data_dir}/full-source-scored-data.jsonl', nrows=args.end_idx, lines=True
+    ).iloc[args.start_idx:]
     article_d = load_from_disk('all-coref-resolved')
 
-    #process the data into right format: article with annotated sentences
-    filtered_article_d = article_d.filter(lambda x: x['article_url'] in set(source_df['article_url']), num_proc=10)
+    # process the data into right format: article with annotated sentences
+    a_urls_lookup = set(source_df['article_url'])
+    filtered_article_d = article_d.filter(lambda x: x['article_url'] in a_urls_lookup, num_proc=10)
     disallowed_quote_types = set(['Other', 'Background/Narrative', 'No Quote'])
+    disallowed_sources = set(['journalist', 'passive-voice'])
     sentences_with_quotes = (
         filtered_article_d
             .to_pandas()
@@ -56,12 +75,17 @@ if __name__ == "__main__":
 
     sentences_with_quotes = (sentences_with_quotes
         .assign(attributions=lambda df:
-                df.apply(lambda x: x['attributions'] if x['quote_type'] not in disallowed_quote_types else np.nan, axis=1)
+            df.apply(lambda x:
+                       x['attributions'] if (
+                               (len(x['attributions']) < 50) or
+                               (x['quote_type'] not in disallowed_quote_types) or
+                               (x['attributions'] not in disallowed_sources)) else np.nan, axis=1)
         )
     )
 
     #store each article_url, annoted_sentences pair
-    articles = []
+    messages = []
+    urls = []
     for url in sentences_with_quotes['article_url'].unique():
         one_article = (
             sentences_with_quotes
@@ -69,40 +93,37 @@ if __name__ == "__main__":
                 .reset_index(drop=True)
                 )
 
-        json_str = one_article[['sent_lists', 'attributions']].to_json(lines=True, orient='records')
-        articles.append((url, json_str))
-
-    #load the model
-    model = load_model("meta-llama/Meta-Llama-3-70B-Instruct")
-
-    # loop through and create prompts for each article
-    for article in articles:
-        json_str = article[1]
-        url = article[0]
+        json_str = (
+            one_article[['sent_lists', 'attributions']]
+            .rename(columns={'sent_lists': 'sentence', 'attributions': 'source'})
+            .to_json(lines=True, orient='records')
+        )
 
         prompt = f"""
-                Here is a news article, with each sentence annotated according to the source of it's information:
-                ```
-                {json_str}
-                ```
+                        Here is a news article, with each sentence annotated according to the source of it's information:
+                        ```
+                        {json_str}
+                        ```
 
-                Please summarize each of our source annotations. Tell me in one paragraph per source: (1) who the source is (2) what informational content they provide to the article. 
-                Only rely on the annotations I have provided, don't identify additional sources. Generate only ONE summary per source. That is, summarize the SAME source if it occurs in multiple source annotations.
-            """
+                        Please summarize each of our source annotations. Tell me in one paragraph per source: (1) who the source is (2) what informational content they provide to the article. 
+                        Only rely on the annotations I have provided, don't identify additional sources. Generate only ONE summary per source. That is, summarize the SAME source if it occurs in multiple source annotations.
+                    """
 
-        message = [
-            {
-                "role": "system",
-                "content": "You are an experienced journalist.",
-            },
+        messages.append( prompt)
+        urls.append(url)
 
-            {"role": "user",
-            "content": prompt},
-            ]
+    #load the model
+    model = load_model(args.model)
+    outputs = model.generate(messages, SamplingParams(temperature=0.8, top_p=0.95))
 
-        response = infer(model, message)
-
-        with open('sources_data_70b.txt', 'a') as file:
+    if args.start_idx is None:
+        args.start_idx = 0
+    if args.end_idx is None:
+        args.end_idx = len(urls)
+    fname = f'sources_data_70b__{args.start_idx}_{args.end_idx}.txt'
+    with open(fname, 'w') as file:
+        for url, output in zip( urls, outputs):
+            response = output.outputs[0].text
             file.write(url)
             file.write('\n')
             file.write('{')
--- data_vllm_70b.sh
+++ data_vllm_70b.sh
@@ -9,4 +9,42 @@
 
 module load python/3.11
 pip install -r requirements.txt
-python3 data_vllm_70b.py
+python3 data_vllm_70b.py --start_idx 0 --end_idx 10_000
+python3 data_vllm_70b.py --start_idx 10_000 --end_idx 20_000
+python3 data_vllm_70b.py --start_idx 20_000 --end_idx 30_000
+python3 data_vllm_70b.py --start_idx 30_000 --end_idx 40_000
+python3 data_vllm_70b.py --start_idx 40_000 --end_idx 50_000
+python3 data_vllm_70b.py --start_idx 50_000 --end_idx 60_000
+python3 data_vllm_70b.py --start_idx 60_000 --end_idx 70_000
+python3 data_vllm_70b.py --start_idx 70_000 --end_idx 80_000
+python3 data_vllm_70b.py --start_idx 80_000 --end_idx 90_000
+python3 data_vllm_70b.py --start_idx 90_000 --end_idx 100_000
+python3 data_vllm_70b.py --start_idx 100_000 --end_idx 110_000
+python3 data_vllm_70b.py --start_idx 110_000 --end_idx 120_000
+python3 data_vllm_70b.py --start_idx 120_000 --end_idx 130_000
+python3 data_vllm_70b.py --start_idx 130_000 --end_idx 140_000
+python3 data_vllm_70b.py --start_idx 140_000 --end_idx 150_000
+python3 data_vllm_70b.py --start_idx 150_000 --end_idx 160_000
+python3 data_vllm_70b.py --start_idx 160_000 --end_idx 170_000
+python3 data_vllm_70b.py --start_idx 170_000 --end_idx 180_000
+python3 data_vllm_70b.py --start_idx 180_000 --end_idx 190_000
+python3 data_vllm_70b.py --start_idx 190_000 --end_idx 200_000
+python3 data_vllm_70b.py --start_idx 200_000 --end_idx 210_000
+python3 data_vllm_70b.py --start_idx 210_000 --end_idx 220_000
+python3 data_vllm_70b.py --start_idx 220_000 --end_idx 230_000
+python3 data_vllm_70b.py --start_idx 230_000 --end_idx 240_000
+python3 data_vllm_70b.py --start_idx 240_000 --end_idx 250_000
+python3 data_vllm_70b.py --start_idx 250_000 --end_idx 260_000
+python3 data_vllm_70b.py --start_idx 260_000 --end_idx 270_000
+python3 data_vllm_70b.py --start_idx 270_000 --end_idx 280_000
+python3 data_vllm_70b.py --start_idx 280_000 --end_idx 290_000
+python3 data_vllm_70b.py --start_idx 290_000 --end_idx 300_000
+python3 data_vllm_70b.py --start_idx 300_000 --end_idx 310_000
+python3 data_vllm_70b.py --start_idx 310_000 --end_idx 320_000
+python3 data_vllm_70b.py --start_idx 320_000 --end_idx 330_000
+python3 data_vllm_70b.py --start_idx 330_000 --end_idx 340_000
+python3 data_vllm_70b.py --start_idx 340_000 --end_idx 350_000
+python3 data_vllm_70b.py --start_idx 350_000 --end_idx 360_000
+python3 data_vllm_70b.py --start_idx 360_000 --end_idx 370_000
+python3 data_vllm_70b.py --start_idx 370_000 --end_idx 380_000
+python3 data_vllm_70b.py --start_idx 380_000 --end_idx 390_000

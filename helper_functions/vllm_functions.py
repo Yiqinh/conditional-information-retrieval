856f19ebe9a1546b5bf51887194c1647a36dd7f0
Yiqin <yiqinhuang@berkeley.edu>
2024-07-15 15:23:41 -0700

    implement batch logic

--- helper_functions/vllm_functions.py
+++ helper_functions/vllm_functions.py
@@ -3,6 +3,7 @@ from vllm import LLM,  SamplingParams
 from transformers import AutoTokenizer
 import os
 import unicodedata
+from tqdm.auto import tqdm
 
 """
 Setting up the environment variables:
@@ -31,7 +32,7 @@ def load_model(model_id: str):
     return model
 
 
-def infer(model, messages, model_id, temperature=0.1, max_tokens=512):
+def infer(model, messages, model_id, temperature=0.1, max_tokens=512, batch_size=100):
     """
     Args:
     model: model returned by vllm.LLM()
@@ -51,21 +52,29 @@ def infer(model, messages, model_id, temperature=0.1, max_tokens=512):
     model_id: Name of model as it appears on huggingface
     temperature, max_tokens: model hyperparameters
     """
+
+    message_batches = []
     formatted_messages = []
+    message_batches.append(formatted_messages)
+
     tokenizer = AutoTokenizer.from_pretrained(model_id)
 
     for message in messages:
+        if len(formatted_messages) >= batch_size:
+            message_batches.append(formatted_messages)
+            formatted_messages = []
+            
         formatted_prompt = tokenizer.apply_chat_template(message, tokenize=False, add_generation_prompt=True)
         formatted_messages.append(formatted_prompt)
 
     sampling_params = SamplingParams(temperature=temperature, max_tokens=max_tokens)
-    outputs = model.generate(formatted_messages, sampling_params)
-
-    #return the outputs.
     res = []
-    for output in outputs:
-        generated_text = output.outputs[0].text
-        generated_text = unicodedata.normalize('NFKC', generated_text)
-        res.append(generated_text)
-        
+
+    for batch in tqdm(message_batches):
+        outputs = model.generate(batch, sampling_params)
+        for output in outputs:
+            generated_text = output.outputs[0].text
+            generated_text = unicodedata.normalize('NFKC', generated_text)
+            res.append(generated_text)
+
     return res

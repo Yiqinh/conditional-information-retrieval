1f55fc72ea67127b1f811553e3552f8236121812
Yiqin <yiqinhuang@berkeley.edu>
2024-07-22 15:11:21 -0700

    update

--- source_retriever/base_query_launcher.sh
+++ source_retriever/base_query_launcher.sh
@@ -7,6 +7,6 @@ cd /project/jonmay_231/spangher/Projects/conditional-information-retrieval
 total=300000
 for ((i=0; i<total; i+=30000)); do
     end_idx=$((i + 30000))
-    sbatch source_retriever/base_query.sh --start_idx "${i}" --end_idx "${end_idx}"
+    sbatch source_retriever/base_query.sh ${i} ${end_idx}
 done
 

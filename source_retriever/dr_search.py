6489191d172ef213ec6971514098e37310977dca
Yiqin <yiqinhuang@berkeley.edu>
2024-07-24 15:36:56 -0700

    dr search test

--- source_retriever/dr_search.py
+++ source_retriever/dr_search.py
@@ -1,23 +1,29 @@
 from dense_retriever import MyDenseRetriever
 import json
+import os
+import logging
+here = os.path.dirname(os.path.abspath(__file__))
+    
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(lineno)d - %(message)s",
+    datefmt="%m/%d/%Y %H:%M:%S",
+    level=logging.INFO,
+)
 
+if __name__ == "__main__":
 
-dr = MyDenseRetriever.load("new-index")
+    dr = MyDenseRetriever.load("all_baseline-sources")
 
-queries = [
-    {'url': "www.google.com",
-     'text': "This is a search engine that is the most popular."},
-    {'url': "www.bing.com",
-     'text': "This is another search engine that is not so popular."}
-]
+    f = os.path.join(os.path.dirname(here), 'baseline_queries', 'test_set', 'test_articles.json')
+    with open(here, 'r') as file:
+        articles = json.load(file)
+        for article in articles:
+            my_query = article['query']
+            break
+             
 
-collections = {}
-for summary in queries:
-    collections[summary['url']] = dr.search(query = summary['text'])
-
-print(collections)
-
-fname = "scoreOutput.json"
-
-with open(fname, 'w') as json_file:
-        json.dump(collections, json_file)
+    res = dr.search(
+        query = my_query
+    )
+    print(type(res))
+    print(res)

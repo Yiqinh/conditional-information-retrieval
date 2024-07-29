3c293e71598db66e83375b0d4ae6731748aa3319
Yiqin <yiqinhuang@berkeley.edu>
2024-07-29 16:33:11 -0700

    update job script

--- source_retriever/dr_search.sh
+++ source_retriever/dr_search.sh
@@ -1,19 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --time=40:00:00
+#SBATCH --time=100:00:00
 #SBATCH --gres=gpu:a100:1
-#SBATCH --constraint=a100-80gb
-#SBATCH --mem=200GB
+#SBATCH --mem=100GB
 #SBATCH --cpus-per-gpu=10
-#SBATCH --partition=gpu
+#SBATCH --partition=isi
 
 cd
 cd /project/jonmay_231/spangher/Projects/conditional-information-retrieval
 
-module load conda
-source conda activate CIR
-conda env update -f env.yaml
-pip install -r requirements.txt
+conda activate retriv-py39
 
 python3 source_retriever/dr_search.py --index_name vanilla_baseline

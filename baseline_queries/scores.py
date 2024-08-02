713b39c95a3e944081e94a02c96bb9494d621b87
Yiqin <yiqinhuang@berkeley.edu>
2024-08-02 14:06:19 -0700

    f1 retrieval testing

--- baseline_queries/scores.py
+++ baseline_queries/scores.py
@@ -9,6 +9,7 @@ def get_scores(path: str):
     precision_list = []
     recall_list = []
     f1_list = []
+    num_zero_score = 0
 
     with open(path, 'r') as file:
         articles = json.load(file)
@@ -31,20 +32,23 @@ def get_scores(path: str):
             precision = n / len(y_pred)
 
             if (recall + precision) == 0:
+                num_zero_score += 1
                 f1 = 0
             else:
                 f1 = (2 * precision * recall) / (precision + recall)
             
-            recall_list.append(recall)
-            precision_list.append(precision)
-            f1_list.append(f1)
+                recall_list.append(recall)
+                precision_list.append(precision)
+                f1_list.append(f1)
     
     avg_prec = statistics.mean(precision_list)
     avg_rec = statistics.mean(recall_list)
     avg_f1 = statistics.mean(f1_list)
+
     print("average precision:", avg_prec)
     print("average recall:", avg_rec)
     print("average f1:", avg_f1)
+    print("number of zero correct articles:", num_zero_score)
 
 if __name__ == "__main__":
     get_scores(os.path.join(here, "baseline_results", "retrieved_sources_test_set.json"))

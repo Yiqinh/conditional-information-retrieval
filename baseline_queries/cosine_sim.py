e8628cd521dc1e559e886aca60b67a375b2f496b
Yiqin <yiqinhuang@berkeley.edu>
2024-08-14 22:16:38 -0700

    cosine sim

--- baseline_queries/cosine_sim.py
+++ baseline_queries/cosine_sim.py
@@ -3,7 +3,10 @@
 import os
 import json
 import statistics
+import logging
+
 from sentence_transformers import SentenceTransformer
+from scipy.optimize import linear_sum_assignment
 
 here = os.path.dirname(os.path.abspath(__file__))    
 
@@ -31,10 +34,18 @@ def cos_similarity(path: str):
             y_true_embedding = model.encode(y_true)
 
             similarities = model.similarity(y_pred_embedding, y_true_embedding) #calculates the cosine similarity by default
-            return similarities
 
+            cost = similarities.numpy()
+            row_ind, col_ind = linear_sum_assignment(cost, maximize=True) #hungarian matching
+            
+            total = cost[row_ind, col_ind].sum() #calculate the total cost of the matching
+
+            cos_scores.append(total / min(len(y_true), len(y_pred))) #divide by the number of edges to get average cosine similarity. Append this to the list of cosine scores
+            break
+    
+    return statistics.mean(cos_scores) #return the mean cosine similarity
 
 
 if __name__ == "__main__":
     a = cos_similarity(os.path.join(here, "baseline_results", "retrieved_sources_test_set.json"))
-    print(a)
+    logging.info(f"The average cosine similarity is {a}")
--- baseline_queries/cosine_sim.sh
+++ baseline_queries/cosine_sim.sh
@@ -13,6 +13,7 @@ cd /project/jonmay_231/spangher/Projects/conditional-information-retrieval
 module load conda
 source conda activate CIR
 
+python -m pip install scipy
 pip install -U sentence-transformers
 
 python3 baseline_queries/cosine_sim.py

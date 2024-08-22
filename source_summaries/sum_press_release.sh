0e22e1a814bf12c35ffde1b60f8832b7d9e4b91c
Yiqin <yiqinhuang@berkeley.edu>
2024-08-21 19:28:37 -0700

    summarize pr

---
+++ source_summaries/sum_press_release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --time=40:00:00
+#SBATCH --gres=gpu:4
+#SBATCH --cpus-per-gpu=10
+#SBATCH --mem=400G
+#SBATCH --partition=isi
+
+source /home1/spangher/.bashrc
+
+conda activate /home1/spangher/miniconda3/envs/vllm-py310
+
+cd
+cd /project/jonmay_231/spangher/Projects/conditional-information-retrieval
+
+python3 source_summaries/sum_press_release.py

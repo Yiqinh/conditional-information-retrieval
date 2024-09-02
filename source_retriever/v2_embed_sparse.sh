d28a72d4a2b035f7c38da7ed504c4b2a3325ff80
Yiqin <yiqinhuang@berkeley.edu>
2024-09-02 14:42:30 -0700

    embed v2

--- source_retriever/v2_embed_sparse.sh
+++ source_retriever/v2_embed_sparse.sh
@@ -2,12 +2,10 @@
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH --time=40:00:00
-#SBATCH --gres=gpu:a100:1
-#SBATCH --constraint=a100-80gb
-#SBATCH --mem=200GB
+#SBATCH --gres=gpu:4
 #SBATCH --cpus-per-gpu=10
-#SBATCH --partition=gpu
-
+#SBATCH --mem=100G
+#SBATCH --partition=isi
 
 source /home1/spangher/.bashrc
 conda activate retriv-py39

0c268e86eec2f2b2f501e82254f356860cfa3c7f
Alex Spangher <spangher@usc.edu>
2024-09-11 16:18:22 -0700

    updated

--- source_summaries/data_vllm_70b.py
+++ source_summaries/data_vllm_70b.py
@@ -25,7 +25,7 @@ config_data = json.load(open(f'{proj_dir}/config.json'))
 os.environ['HF_TOKEN'] = config_data["HF_TOKEN"]
 os.environ['HF_HOME'] = HF_HOME
 os.environ['VLLM_WORKER_MULTIPROC_METHOD'] = 'spawn'
-os.environ['VLLM_ALLOW_LONG_MAX_MODEL_LEN'] = 1
+os.environ['VLLM_ALLOW_LONG_MAX_MODEL_LEN'] = '1'
 
 BATCH_SIZE = 50
 INFO_PROMPT = """

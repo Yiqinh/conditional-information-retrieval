ecfb3edef4c1874219441774bb6f6283f0eed483
Yiqin <yiqinhuang@berkeley.edu>
2024-09-18 00:04:09 -0700

    query info cosim

--- narrative_function/centrality_cosim.sh
+++ narrative_function/centrality_cosim.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --time=40:00:00
-#SBATCH --gres=gpu:a100:1
-#SBATCH --mem=40GB
-#SBATCH --cpus-per-gpu=10
-#SBATCH --partition=gpu
+#SBATCH --time=80:00:00
+#SBATCH --gres=gpu:2
+#SBATCH --cpus-per-gpu=5
+#SBATCH --mem=40G
+#SBATCH --partition=isi
 
 cd
 cd /project/jonmay_231/spangher/Projects/conditional-information-retrieval
@@ -16,4 +16,4 @@ source conda activate CIR
 python -m pip install scipy
 pip install -U sentence-transformers
 
-python3 baseline_queries/cosine_sim.py
+python3 narrative_function/centrality_cosim.py

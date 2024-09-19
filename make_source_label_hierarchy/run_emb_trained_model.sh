eb4530143294898eea453a331846800022b0d64c
Alex Spangher <spangher@usc.edu>
2024-09-18 18:54:09 -0700

    updated

---
+++ make_source_label_hierarchy/run_emb_trained_model.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+#SBATCH --job-name=run_emb_trained_model
+#SBATCH --time=10:00:00
+#SBATCH --partition=isi
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=150G
+
+python -c """
+from utils_basic import process_source_data
+from sentence_transformers import SentenceTransformer
+import numpy as np 
+
+source_df = process_source_data(data_dir='/project/jonmay_231/spangher/Projects/conditional-information-retrieval/source_summaries/v2_err_raw')
+to_label = source_df.loc[lambda df: ~df['Is_Error']]['Narrative Function'].dropna().tolist()
+model = SentenceTransformer("/scratch1/spangher/conditional-information-retrieval/make_source_label_hierarchy/models/mpnet-BASE-all-nli-triplet/trained-model/")
+
+embs = model.encode(to_label, batch_size=1, show_progress_bar=True)
+np.savez_compressed('/scratch1/spangher/conditional-information-retrieval/make_source_label_hierarchy/similarity_training_data/all_labeled_embeddings_trained_model.npz', embs)
+"""

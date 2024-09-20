039a2ef219977bdf7b9b13c91857d884593bfdcb
Liheng Lai <liheng@berkeley.edu>
2024-09-20 14:10:05 -0700

    error handling

--- notebooks/dprEval.py
+++ notebooks/dprEval.py
@@ -20,46 +20,28 @@ with open(dev_filename, 'r') as f:
 # document_store = InMemoryDocumentStore()
 document_store = FAISSDocumentStore(sql_url="sqlite:///", faiss_index_factory_str="Flat")
 # documents = []
-error_count = 0
-for article in tqdm(articles, desc="creating source txt folder"):
-    for source in article['sources']:
-        source_name = source['Name']
-        source_text = source['Information']
-        url = article['url']
-        file_name = source_name.replace(" ", "_")
-        file_name = source_name.replace("/", "_")
-        try:
-            with open(f"{data_dir}/{file_name}.txt", 'w') as source_file:
-                source_file.write(url + "###" + source_name + "###" + source_text)
-        except Exception as e:
-            print(f"An error occurred while writing the file: {e}")
-            error_count += 1
+# error_count = 0
+# for article in tqdm(articles, desc="creating source txt folder"):
+#     for source in article['sources']:
+#         source_name = source['Name']
+#         source_text = source['Information']
+#         url = article['url']
+#         file_name = source_name.replace(" ", "_")
+#         file_name = source_name.replace("/", "_")
+#         try:
+#             with open(f"{data_dir}/{file_name}.txt", 'w') as source_file:
+#                 source_file.write(url + "###" + source_name + "###" + source_text)
+#         except Exception as e:
+#             print(f"An error occurred while writing the file: {e}")
+#             error_count += 1
 
-print("error count", error_count)
+# print("error count", error_count)
 
 
 print("converting files to docs...")
 docs = convert_files_to_docs(dir_path=data_dir)
 print("updating document store...")
 document_store.write_documents(docs)
-
-
-# document_store.write_documents(documents)
-
-# embedding_model = EmbeddingRetriever(
-#     document_store=document_store,
-#     embedding_model="facebook/dpr-ctx_encoder-single-nq-base",
-#     use_gpu=True
-# )
-
-# print("updating embeddings")
-# document_store.update_embeddings(retriever=embedding_model)
-# print("embedding update completed")
-
-# retriever = EmbeddingRetriever(
-#         document_store=document_store,
-#         embedding_model=model, # this is my custom-trained model
-#     )
 
 
 retriever = EmbeddingRetriever(
@@ -81,10 +63,16 @@ for article in tqdm(articles):
     dr_result = {}
     
     for k in topk:
-        url, name, text = k.content.split("###")
+        try:
+            url, name, text = k.content.split("###")
+        except Exception as e:
+            
+            print(k.content)
+
         id = url + "#" + name
         dr_result['id'] = id
         dr_result['text'] = text
+
     one_article = {}
     one_article['url'] = article['url']
     one_article['sources'] = article['sources']

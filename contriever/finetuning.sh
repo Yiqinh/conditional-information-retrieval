8f908614aca3bbc96c03776beeb2ca950c124f61
Liheng Lai <liheng@berkeley.edu>
2024-09-23 10:04:53 -0700

    udpated path

--- contriever/finetuning.sh
+++ contriever/finetuning.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --time=80:00:00
-#SBATCH --gres=gpu:4
-#SBATCH --cpus-per-gpu=5
-#SBATCH --mem=150G
+#SBATCH --time=10:00:00
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=400G
 #SBATCH --partition=isi
 
 cd /project/jonmay_231/spangher/Projects/conditional-information-retrieval/contriever

dc0898064ccb893a48f3cf79dd43f4b0079e1605
Liheng Lai <liheng@berkeley.edu>
2024-09-26 11:46:05 -0700

    new embedding

--- notebooks/dprEmbedding.py
+++ notebooks/dprEmbedding.py
@@ -13,6 +13,7 @@ from haystack.utils import convert_files_to_docs
 
 save_dir = "../trained_model"
 data_dir = "/project/jonmay_231/spangher/Projects/conditional-information-retrieval/fine_tuning/docs"
+mapfile = "/project/jonmay_231/spangher/Projects/conditional-information-retrieval/fine_tuning/mapping.json"
 dev_filename = "/project/jonmay_231/spangher/Projects/conditional-information-retrieval/source_summaries/v2_info_parsed/combined_test_prompt1_v2.json"
 index_file = "/project/jonmay_231/spangher/Projects/conditional-information-retrieval/fine_tuning/test.index"
 # Load development data
@@ -23,52 +24,67 @@ with open(dev_filename, 'r') as f:
 document_store = FAISSDocumentStore(sql_url="sqlite:///", faiss_index_factory_str="Flat")
 reloaded_retriever = DensePassageRetriever.load(load_dir=save_dir, document_store=document_store)
 
+mapping = {}
+dim = 768  # Dimension of the vectors
+index = faiss.IndexFlatL2(dim)
+index = faiss.IndexIDMap(index)
+
 # Write documents to the document store
 print("creating source txt folder...")
 file_idx = 0
 for article in tqdm(articles, desc="creating source txt folder"):
     for source in article['sources']:
         source_text = source['Information']
+        doc_vector = reloaded_retriever.embed_documents(source_text)
         with open(f"{data_dir}/{file_idx}.txt", 'w') as source_file:
             source_file.write(source_text)
+            mapping[file_idx] = source_text
+            index.add_with_ids(doc_vector, file_idx)
         file_idx += 1
 
-print("converting files to docs...")
-docs = convert_files_to_docs(dir_path=data_dir)
-print("writing to document store...")
-document_store.write_documents(docs)
+print("saving mapping file")
+with open(mapfile, 'w') as f:
+    json.dump(mapping, f, indent=4)
 
-# Embed documents
-tmp = reloaded_retriever.embed_documents(docs)
+print("saving index")
+faiss.write_index(index, index_file)
 
-def create_index(vector_dim):
-    """Create a FAISS index for the given vector dimension."""
-    index = faiss.IndexFlatL2(vector_dim)  # Using L2 distance for similarity
-    return index
 
-def add_vectors_to_index(index, vectors):
-    """Add vectors to the FAISS index."""
-    if isinstance(vectors, np.ndarray) and vectors.dtype != np.float32:
-        vectors = vectors.astype(np.float32)
-    index.add(vectors)  # Add vectors to the index
-    print("Vectors added to index!")
+
+
+# print("converting files to docs...")
+# docs = convert_files_to_docs(dir_path=data_dir)
+# print("writing to document store...")
+# document_store.write_documents(docs)
+
+# # Embed documents
+# tmp = reloaded_retriever.embed_documents(docs)
+
+# def create_index(vector_dim):
+#     """Create a FAISS index for the given vector dimension."""
+#     index = faiss.IndexFlatL2(vector_dim)  # Using L2 distance for similarity
+#     return index
+
+# def add_vectors_to_index(index, vectors):
+#     """Add vectors to the FAISS index."""
+#     if isinstance(vectors, np.ndarray) and vectors.dtype != np.float32:
+#         vectors = vectors.astype(np.float32)
+#     index.add(vectors)  # Add vectors to the index
+#     print("Vectors added to index!")
 
 def search_vectors(index, query_vector, k):
     """Search the index for the k nearest vectors to the query."""
     D, I = index.search(np.array([query_vector], dtype=np.float32), k)  # Perform the search
     return D, I  # Distances and indices of the nearest vectors
 
-def get_index(source, index):
-    query_vector = reloaded_retriever.embed_queries([source])[0]
-    return index.search(np.array([query_vector], dtype=np.float32), 1)[0][0]
+# def get_index(source, index):
+#     query_vector = reloaded_retriever.embed_queries([source])[0]
+#     return index.search(np.array([query_vector], dtype=np.float32), 1)[0][0]
 
-# Set up the FAISS index
-dim = 768  # Dimension of the vectors
-index = create_index(dim)
 
 
 # Adding vectors to the index
-add_vectors_to_index(index, tmp)
+# add_vectors_to_index(index, tmp)
 
 # Simulating a query vector (for example purposes)
 question = "what is the square root of 144?"
@@ -78,5 +94,5 @@ distances, indices = search_vectors(index, query_vector, 10)
 print("Nearest neighbors: ", indices)
 print("Distances: ", distances)
 
-faiss.write_index(index, index_file)
+
 

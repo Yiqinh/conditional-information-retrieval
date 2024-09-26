5c4a9c945b000b5db36233370d14a96f8d5fadb1
Liheng Lai <liheng@berkeley.edu>
2024-09-26 01:03:55 -0700

    contrive new code

--- contriever/finetuning.sh
+++ contriever/finetuning.sh
@@ -1,20 +1,20 @@
-#!/bin/bash
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --time=10:00:00
+#SBATCH --time=8:00:00
 #SBATCH --gres=gpu:4
-#SBATCH --cpus-per-gpu=10
-#SBATCH --mem=200G
-#SBATCH --partition=isi
-
-srun -N 1 --cpus-per-task=32 --gres=gpu:4 --partition=sched_mit_psfc_gpu_r8 --pty bash -i
-
-
+#SBATCH --cpus-per-task=50
+#SBATCH --mem=400G
+#SBATCH --partition=sched_mit_psfc_gpu_r8
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=liheng@berkeley.edu
 
 
-cd /project/jonmay_231/spangher/Projects/conditional-information-retrieval/contriever
+cd /pool001/spangher/alex/conditional-information-retrieval/contriever
 
+conda init
 module load conda
 source conda activate contriever
-pip install torch
+
+export OMP_NUM_THREADS=50
+
 python3 finetuning.py

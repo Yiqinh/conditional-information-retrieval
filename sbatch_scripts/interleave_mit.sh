690145f37af4790c7eb41b9e761fa4e5247d65a5
Yiqin <yiqinhuang@berkeley.edu>
2024-09-25 23:27:35 -0700

    mit server

---
+++ sbatch_scripts/interleave_mit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --time=80:00:00
+#SBATCH --gres=gpu:4
+#SBATCH --cpus-per-task=100
+#SBATCH --mem=400G
+#SBATCH --partition=sched_mit_psfc_gpu_r8
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=yiqinhuang@berkeley.edu
+
+cd /pool001/spangher/alex/conditional-information-retrieval
+
+conda activate py39-retrieve-vllm
+
+export OMP_NUM_THREADS=50
+
+python3 interleaving/interleave.py

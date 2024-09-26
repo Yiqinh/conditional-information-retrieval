bc11c2e38fa7c739af5d387e96cd83cce5d9d44c
Yiqin <yiqinhuang@berkeley.edu>
2024-09-25 23:34:26 -0700

    mit server

--- sbatch_scripts/interleave_mit.sh
+++ sbatch_scripts/interleave_mit.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --time=80:00:00
+#SBATCH --time=8:00:00
 #SBATCH --gres=gpu:4
-#SBATCH --cpus-per-task=100
+#SBATCH --cpus-per-task=10
 #SBATCH --mem=400G
 #SBATCH --partition=sched_mit_psfc_gpu_r8
 #SBATCH --mail-type=ALL

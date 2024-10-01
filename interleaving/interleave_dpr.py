2bb9ffdad4a22f219ffbb8d202c108dfe1f70bc0
Liheng Lai <liheng@berkeley.edu>
2024-09-30 21:52:43 -0700

    hopefully working scirpt

--- interleaving/interleave_dpr.py
+++ interleaving/interleave_dpr.py
@@ -127,9 +127,9 @@ if __name__ == "__main__":
     #LOAD THE LLM
     helper_dir = os.path.join(os.path.dirname(here), 'helper_functions')
     sys.path.append(helper_dir)
-    from vllm_functions import load_model, infer
+    # from vllm_functions import load_model, infer
 
-    LLM_model = load_model(args.model)
+    # LLM_model = load_model(args.model)
     model = pipeline("text-generation", model=args.model)
     #response = infer(model=my_model, messages=messages, model_id=args.model, batch_size=100)
     print("Loaded the LLM Model...")

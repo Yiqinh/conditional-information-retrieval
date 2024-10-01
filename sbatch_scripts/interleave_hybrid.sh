2e18f05fcb7681309d608214e7f4f7f749bc52d5
Yiqin <yiqinhuang@berkeley.edu>
2024-10-01 01:27:49 -0700

    update

--- sbatch_scripts/interleave_hybrid.sh
+++ sbatch_scripts/interleave_hybrid.sh
@@ -3,7 +3,7 @@
 #SBATCH -n 1
 #SBATCH --time=8:00:00
 #SBATCH --gres=gpu:4
-#SBATCH --cpus-per-task=100
+#SBATCH --cpus-per-task=50
 #SBATCH --mem=400G
 #SBATCH --partition=sched_mit_psfc_gpu_r8
 
@@ -15,7 +15,7 @@ cd /pool001/spangher/alex/conditional-information-retrieval
 conda init
 source conda activate py39-retrieve-vllm
 
-export OMP_NUM_THREADS=100
+export OMP_NUM_THREADS=50
 export VLLM_WORKER_MULTIPROC_METHOD=spawn
 
 python3 interleaving/interleave_v2_hybrid.py --start_idx=${start_idx} --end_idx=${end_idx} --iterations=20

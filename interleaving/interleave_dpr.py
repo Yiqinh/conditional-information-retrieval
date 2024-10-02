5c9d84973605a118d0e85cf6e67ce0378c8e7b30
Liheng Lai <liheng@berkeley.edu>
2024-10-01 20:38:39 -0700

    no more transformers

--- interleaving/interleave_dpr.py
+++ interleaving/interleave_dpr.py
@@ -30,6 +30,8 @@ logging.basicConfig(
 HF_LLAMA = "/project/jonmay_231/spangher/huggingface_cache/models--meta-llama--Meta-Llama-3-70B-Instruct/snapshots/7129260dd854a80eb10ace5f61c20324b472b31c"
 save_dir = "../trained_model"
 
+print("loading model")
+
 dr = DensePassageRetriever.load(load_dir=save_dir, document_store=None)
 
 def search_vectors(index, query_vector, k):

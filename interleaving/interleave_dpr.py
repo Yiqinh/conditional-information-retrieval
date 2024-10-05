6dd6a615567bcd1fe553666ba96dd83f3369b910
Liheng Lai <liheng@berkeley.edu>
2024-10-05 13:43:07 -0700

    mit interleave script

--- interleaving/interleave_dpr.py
+++ interleaving/interleave_dpr.py
@@ -29,7 +29,7 @@ logging.basicConfig(
 )
 
 HF_dir = "/project/jonmay_231/spangher/huggingface_cache"
-save_dir = "/project/jonmay_1426/spangher/conditional-information-retrieval/trained_model"
+save_dir = "/pool001/spangher/alex/conditional-information-retrieval/trained_model"
 
 
 def search_vectors(index, query_vector, k):
@@ -134,7 +134,7 @@ if __name__ == "__main__":
     os.environ['RETRIV_BASE_PATH'] = retriv_cache_dir
 
     
-    index_file = "/project/jonmay_231/spangher/Projects/conditional-information-retrieval/fine_tuning/test.index"
+    index_file = "/pool001/spangher/alex/conditional-information-retrieval/fine_tuning/test.index"
 
     print("loading model...")
 

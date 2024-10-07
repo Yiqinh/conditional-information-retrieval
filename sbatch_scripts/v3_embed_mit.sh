a789c366e03656f57503903b87d960fed4df5e97
Yiqin <yiqinhuang@berkeley.edu>
2024-10-07 14:50:02 -0700

    v3 embed

---
+++ sbatch_scripts/v3_embed_mit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --time=8:00:00
+#SBATCH --gres=gpu:4
+#SBATCH --cpus-per-task=50
+#SBATCH --mem=400G
+#SBATCH --partition=sched_mit_psfc_gpu_r8
+
+cd /pool001/spangher/alex/conditional-information-retrieval
+conda activate py39-retrieve-vllm
+
+export OMP_NUM_THREADS=50
+
+python3 source_retriever/v3_embed_dense.py
--- source_retriever/v3_embed_dense.py
+++ source_retriever/v3_embed_dense.py
@@ -18,10 +18,10 @@ if __name__ == '__main__':
     parser.add_argument("--device", type=str, default='cuda' if torch.cuda.is_available() else 'cpu', help="Device to use for inference")
     # defaults and configs
     parser.add_argument("--retriv_cache_dir", type=str, default=here, help="Path to the directory containing indices")
-    parser.add_argument("--huggingface_cache_dir", type=str, default='/project/jonmay_231/spangher/huggingface_cache', help="Path to the directory containing HuggingFace cache")
+    #parser.add_argument("--huggingface_cache_dir", type=str, default='/project/jonmay_231/spangher/huggingface_cache', help="Path to the directory containing HuggingFace cache")
     parser.add_argument('--embedding_dim', type=int, default=None, help="The dimension of the embeddings")
     parser.add_argument("--max_seq_length", type=int, default=None, help="Maximum sequence length for the model")
-    parser.add_argument("--batch_size_to_index", type=int, help="Batch size for indexing", default=1)
+    parser.add_argument("--batch_size_to_index", type=int, help="Batch size for indexing", default=512)
     args = parser.parse_args()
 
     #set huggingface token
@@ -29,46 +29,35 @@ if __name__ == '__main__':
     os.environ['HF_TOKEN'] = config_data["HF_TOKEN"]
 
     #set the proper huggingface cache directory
-    hf_cache_dir = args.huggingface_cache_dir
-    os.environ['HF_HOME'] = hf_cache_dir
-    logging.info(f"Setting environment variables: HF_HOME={hf_cache_dir}")
+    # hf_cache_dir = args.huggingface_cache_dir
+    # os.environ['HF_HOME'] = hf_cache_dir
+    # logging.info(f"Setting environment variables: HF_HOME={hf_cache_dir}")
 
     # needs to be imported here to make sure the environment variables are set before
     # the retriv library sets certain defaults
     from dense_retriever import MyDenseRetriever
-
     #sets the retriv base path
     retriv_cache_dir = args.retriv_cache_dir
     logging.info(f"Setting environment variables: RETRIV_BASE_PATH={retriv_cache_dir}")
     os.environ['RETRIV_BASE_PATH'] = retriv_cache_dir
 
     #building the collection of sources
-    info_dir = os.path.join(os.path.dirname(here), "source_summaries", "v2_info_parsed")
+    file_path = os.path.join(here, "v3_combined_ALL.json")
+    with open(file_path, 'r') as json_file:
+        articles = json.load(json_file)
 
-    f = open(os.path.join(info_dir, "v2_test_set.json"))
-    test_data = json.load(f)
-
-    test_sources = []
-    for article in test_data:
-        for source in article['sources']:
-            if type(source['Information']) == str and type(source['Name']) == str:
-                formatted_source = {"id": article['url'] + "#" + source['Name'], "text": source['Information']}
-                test_sources.append(formatted_source)
-
-    f = open(os.path.join(info_dir, "v2_train_set.json"))
-    train_data = json.load(f)
-
-    train_sources = []
-    for article in train_data:
-        for source in article['sources']:
-            if type(source['Information']) == str and type(source['Name']) == str:
-                formatted_source = {"id": article['url'] + "#" + source['Name'], "text": source['Information']}
-                train_sources.append(formatted_source)
-
-    # set up index
-    all_sources = train_sources + test_sources
+    all_sources = []
+    for article in articles:
+        for source in article['truth']:
+            one_source = {}
+            id = article['url'] + "#" + source['Name']
+            text = source['Text_embed']
+            one_source['id'] = id
+            one_source['text'] = text
+            all_sources.append(one_source)
+    
     dr = MyDenseRetriever(
-        index_name="v2-ALL-SFR-index",
+        index_name="v3-ALL-SFR-index",
         model=args.embedding_model,
         normalize=True,
         max_length=args.max_seq_length,
@@ -77,7 +66,7 @@ if __name__ == '__main__':
         use_ann=True,
     )
 
-    print("currently indexing ALL sources. len:", len(all_sources))
+    print("currently indexing ALL sources. Length: ", len(all_sources))
     dr.index(
         collection=all_sources,  # File kind is automatically inferred
         batch_size=args.batch_size_to_index,  # Default value
